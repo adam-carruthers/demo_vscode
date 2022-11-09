8d288e215d0e03fa36251139d7062ab3acdc6d60
adam.carruthers1 <adam.carruthers1@nhs.net>
2022-11-09 19:12:49 +0000

    Added pandas for demo

--- demo_vscode/main.py
+++ demo_vscode/main.py
@@ -1,4 +1,5 @@
-print("hello")
+import pandas as pd
+
 
 def calc_full_name():
     """
@@ -14,12 +15,20 @@ def combine_names(first_name, last_name):
     return first_name + last_name
 
 
+def do_pandas_stuff_for_demo():
+    titanic = pd.read_csv("titanic.csv")
+
+    print(titanic.groupby(["Sex", "Survived"]).size())
+
+
 def main():
     print("Hello and welcome to my spectacular program")
 
     full_name = calc_full_name()
 
     print(f"Your name is {full_name}")
+
+    do_pandas_stuff_for_demo()
 
 
 # Only run main() if I run this file directly
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
 from demo_vscode.main import combine_names
 
 def test_combine_names():
-    assert combine_names("John", "Doe") == "John Doe"
+    assert combine_names("John", "Doe") == "JohnDoe"
