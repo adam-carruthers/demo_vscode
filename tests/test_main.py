98419fa5a07e2b80965f7198dc9ce47fe19ed93d
adam.carruthers1 <adam.carruthers1@nhs.net>
2022-11-16 23:22:32 +0000

    Added more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,9 @@
 from demo_vscode.main import combine_names
 
+
 def test_combine_names():
     assert combine_names("John", "Doe") == "JohnDoe"
+
+
+def test_combine_other_names():
+    assert combine_names("Foo", "Bar") == "FooBar"
